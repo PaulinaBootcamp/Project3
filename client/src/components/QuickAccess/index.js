f83a8e05249f5af6a188dcf1c9cfe35e5546519f
PaulinaBootcamp <paulinaoshafi@gmail.com>
2019-08-14 01:35:13 -0400

    quick access bar added

--- client/src/components/QuickAccess/index.js
+++ client/src/components/QuickAccess/index.js
@@ -1,17 +1,32 @@
 import React from "react";
 import "./style.css";
+import { Col, Row, Container } from "../Grid";
 
 function QuickAccess() {
   return (
-    <div class="jumbotron text-center">
-      <div class="overlay">
+
+    <div className="jumbotron text-center">
+      <div className="overlay">
       </div>
-      <div class="background-image">
-      </div>
-      <div class="caption">
-        <h1>Mongo Scraper</h1>
-        <p>New York Times Edition</p>
-      </div>
+      <div className="background-image">
+            <Container>
+        <Row>
+          <Col size="md-4">              
+                  <div><a href="#" class="btn btn-info quick-category-btn">17</a></div>
+                  <h2>GRADING QUE</h2>            
+          </Col>
+          <Col size="md-4">              
+                  <div><a href="#" class="btn btn-info quick-category-btn">8</a></div>
+                  <h2>OVERDUE</h2>            
+          </Col>
+          <Col size="md-4">              
+                  <div><a href="#" class="btn btn-info quick-category-btn">3</a></div>
+                  <h2>OPEN ITEMS</h2>            
+          </Col>
+        </Row>
+
+      </Container></div>
+
     </div>
   );
 }
