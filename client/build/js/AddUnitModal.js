b01574ce0c8ace441de0cac86f4ccee0d9390bab
PaulinaBootcamp <paulinaoshafi@gmail.com>
2019-08-29 19:24:35 -0400

    removed node_modules and added .gitignote

--- client/build/js/AddUnitModal.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    console.log("Testing")
-            $("#addUnits").append(`
-                <!-- Button to trigger add Modal -->
-                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#addByUPC">Add Unit TEST</button>
-            `);
-});
